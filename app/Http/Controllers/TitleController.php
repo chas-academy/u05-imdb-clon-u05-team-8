84acb0206e4c516dd69f2d7ad10c326ea2c098d2
JonasLundChas <jonas.lund@chasacademy>
2021-02-11 07:38:06 +0100

    Adds Title model

---
+++ app/Http/Controllers/TitleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Title;
+use Illuminate\Http\Request;
+
+class TitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        echo(Title::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Title $title)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Title $title)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Title $title)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Title $title)
+    {
+        //
+    }
+}
---
+++ database/seeders/TitlesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Title;
+
+class TitlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Title::create(array('name' =>'La Jetee'));
+        Title::create(array('name' =>'Forrest Gump'));
+        Title::create(array('name' =>'Matrix'));
+    }
+}
