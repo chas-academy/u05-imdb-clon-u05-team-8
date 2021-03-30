ef9136da34f5d040c15a7d65947f0f228f6dba0a
sandstedterik <erik.k.sandstedt@gmail.com>
2021-03-30 20:25:36 +0200

    Adds responsive style to navbar and styles to the movie & tv-show content on frontpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,37 +20,36 @@
                 &nbsp;<a class=" text-blue-500 underline" href="{{ url('/').'/genre/'.$g->id}}">{{$g->name}}</a>&nbsp;&nbsp;
         @endforeach
 
-        <nav class="flex items-center justify-between max-w-7xl mx-auto sm:px-6 lg:px-8 pl-6 mt-16">
-        <ul class="flex">
-            <li class="mr-16 text-3xl">
-              <a class="text-gray-500 hover:text-gray-800" href="/">Home</a>
-            </li>
-            <li class="mr-16 text-3xl">
-              <a class="text-gray-500 hover:text-gray-800" href="#">About Us</a>
-            </li>
-            <li class="mr-16 text-3xl">
-              <a class="text-gray-500 hover:text-gray-800" href="#">New Movies</a>
-            </li>
-            <li class="mr-16 text-3xl">
-              <a class="text-gray-500 hover:text-gray-800" href="#">Top Movies</a>
-            </li>
-            <li class="mr-16 text-3xl">
-              <a class="text-gray-500 hover:text-gray-800" href="#">Watchlist</a>
-            </li>
-          </ul>
+        <nav class=" max-w-5xl mx-auto mt-16 rounded-full py-3 bg-gray-800">
+            <ul class="flex items-center justify-between ml-8">
+                <li class="lg:mr-14 mr-6 lg:text-3xl md:text-2xl text-xl">
+                <a class="text-gray-300 hover:underline hover:text-gray-200" href="/">Home</a>
+                </li>
+                <li class="lg:mr-14 mr-6 lg:text-3xl md:text-2xl text-xl">
+                <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">About Us</a>
+                </li>
+                <li class="lg:mr-14 mr-6 lg:text-3xl md:text-2xl text-xl">
+                <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">New Movies</a>
+                </li>
+                <li class="lg:mr-14 mr-6 lg:text-3xl md:text-2xl text-xl">
+                <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">Top Movies</a>
+                </li>
+                <li class="lg:mr-14 mr-6 lg:text-3xl md:text-2xl text-xl">
+                <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">Watchlist</a>
+                </li>
+            </ul>
         </nav>
 
-        <h2 class="lg:text-5xl text-4xl mt-40 text-center ">Coming soon</h2>
-
-        <div class="py-12">
+        <div class="py-12 mt-56">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-28 pl-6">
-                <h3 class="lg:text-4xl text-3xl pb-6 bg-white border-b border-gray-400">New Movies</h3>
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h3 class="lg:text-4xl mb-8 text-3xl pb-6 bg-white border-b-4 border-gray-400">New Movies</h3>
+                <div class="bg-white overflow-hidden sm:rounded-lg">
                     <div class="text-2xl pt-6 bg-white grid grid-cols-5 gap-1">
                         @foreach($titles->slice(0, 5) as $title)
                         <div class="lg:w-40 md:w-28 w-28">
-                            <p class="lg:text-2xl text-lg lg:h-36 h-32 font-semibold">{{$title->name}}</p>
-                            <a class="lg:text-2xl text-lg text-blue-500 underline" href="{{ url('/').'/title/'.$title->id}}">Read more</a>
+                            <p class="lg:text-2xl text-lg h-40 font-semibold">{{$title->name}}</p>                                                    
+                            <a class="lg:text-xl text-lg text-gray-500 hover:text-gray-800 hover:underline" href="{{ url('/').'/title/'.$title->id}}">Read more<svg class="w-4 inline-block align-baseline" fill="currentColor" aria-hidden="true" viewBox="0 -10 24 24" width="24" height="24" focusable="false"><path d="M13.71 4.29l-1.42 1.42 5.3 5.29H3v2h14.59l-5.3 5.29 1.42 1.42 7.7-7.71-7.7-7.71z"></path></svg> 
+                            </a>                               
                         </div>
                         @endforeach
                     </div>
@@ -58,15 +57,16 @@
             </div>
         </div>
 
-        <div class="py-12">
+        <div class="py-12 mt-20">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-28 pl-6">
-                <h3 class="lg:text-4xl text-3xl pb-6 bg-white border-b border-gray-400">New TV-Shows</h3>
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h3 class="lg:text-4xl mb-8 text-3xl pb-6 bg-white border-b-4 border-gray-400">New TV-Shows</h3>
+                <div class="bg-white overflow-hidden sm:rounded-lg">
                     <div class="text-2xl pt-6 bg-white grid grid-cols-5 gap-1">
                         @foreach($titles ->slice(0, 5) as $title)
                         <div class="lg:w-40 md:w-28 w-28">
-                            <p class="lg:text-2xl text-lg lg:h-36 h-32 font-semibold">{{$title->name}}</p>
-                            <a class="lg:text-2xl text-lg text-blue-500 underline" href="{{ url('/').'/title/'.$title->id}}">Read more</a>
+                            <p class="lg:text-2xl text-lg h-40 font-semibold">{{$title->name}}</p>                                                    
+                            <a class="lg:text-xl text-lg text-gray-500 hover:text-gray-800 hover:underline" href="{{ url('/').'/title/'.$title->id}}">Read more<svg class="w-4 inline-block align-baseline" fill="currentColor" aria-hidden="true" viewBox="0 -10 24 24" width="24" height="24" focusable="false"><path d="M13.71 4.29l-1.42 1.42 5.3 5.29H3v2h14.59l-5.3 5.29 1.42 1.42 7.7-7.71-7.7-7.71z"></path></svg> 
+                            </a>                               
                         </div>
                         @endforeach
                     </div>
@@ -74,16 +74,16 @@
             </div>
         </div>
 
-
-        <div class="py-12">
+        <div class="py-12 mt-20">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-28 pl-6">
-                <h3 class="lg:text-4xl text-3xl pb-6 bg-white border-b border-gray-400">Movie Tips</h3>
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h3 class="lg:text-4xl mb-8 text-3xl pb-6 bg-white border-b-4 border-gray-400">Movie Tips</h3>
+                <div class="bg-white overflow-hidden sm:rounded-lg">
                     <div class="text-2xl pt-6 bg-white grid grid-cols-5 gap-1">
                         @foreach($titles->slice(0, 5) as $title)
                         <div class="lg:w-40 md:w-28 w-28">
-                            <p class="lg:text-2xl text-lg lg:h-36 h-32 font-semibold">{{$title->name}}</p>
-                            <a class="lg:text-2xl text-lg text-blue-500 underline" href="{{ url('/').'/title/'.$title->id}}">Read more</a>
+                            <p class="lg:text-2xl text-lg h-40 font-semibold">{{$title->name}}</p>                                                    
+                            <a class="lg:text-xl text-lg text-gray-500 hover:text-gray-800 hover:underline" href="{{ url('/').'/title/'.$title->id}}">Read more<svg class="w-4 inline-block align-baseline" fill="currentColor" aria-hidden="true" viewBox="0 -10 24 24" width="24" height="24" focusable="false"><path d="M13.71 4.29l-1.42 1.42 5.3 5.29H3v2h14.59l-5.3 5.29 1.42 1.42 7.7-7.71-7.7-7.71z"></path></svg> 
+                            </a>                               
                         </div>
                         @endforeach
                     </div>
@@ -91,24 +91,22 @@
             </div>
         </div>
 
-
-
-        <div class="py-12">
+        <div class="py-12 mt-20">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-28 pl-6">
-                <h3 class="lg:text-4xl text-3xl pb-6 bg-white border-b border-gray-400">TV-Show Tips</h3>
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h3 class="lg:text-4xl mb-8 text-3xl pb-6 bg-white border-b-4 border-gray-400">TV-Show Tips</h3>
+                <div class="bg-white overflow-hidden sm:rounded-lg">
                     <div class="text-2xl pt-6 bg-white grid grid-cols-5 gap-1">
                         @foreach($titles->slice(0, 5) as $title)
                         <div class="lg:w-40 md:w-28 w-28">
-                            <p class="lg:text-2xl text-lg lg:h-36 h-32 font-semibold">{{$title->name}}</p>
-                            <a class="lg:text-2xl text-lg text-blue-500 underline" href="{{ url('/').'/title/'.$title->id}}">Read more</a>
+                            <p class="lg:text-2xl text-lg h-40 font-semibold">{{$title->name}}</p>                                                    
+                            <a class="lg:text-xl text-lg text-gray-500 hover:text-gray-800 hover:underline" href="{{ url('/').'/title/'.$title->id}}">Read more<svg class="w-4 inline-block align-baseline" fill="currentColor" aria-hidden="true" viewBox="0 -10 24 24" width="24" height="24" focusable="false"><path d="M13.71 4.29l-1.42 1.42 5.3 5.29H3v2h14.59l-5.3 5.29 1.42 1.42 7.7-7.71-7.7-7.71z"></path></svg> 
+                            </a>                               
                         </div>
                         @endforeach
                     </div>
                 </div>
             </div>
         </div>
-
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-28 pl-6">
