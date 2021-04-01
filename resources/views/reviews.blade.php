aa4cd368a1754ced9ad618a5492af50dad568dc4
sandstedterik <erik.k.sandstedt@gmail.com>
2021-04-01 13:11:02 +0200

    Changes layout on reviews.blade to only show the reviews that are approved by admin

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -13,12 +13,35 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+      <nav class=" max-w-5xl mx-auto mt-16 rounded-full py-3 bg-gray-800">
+        <ul class="flex items-center justify-between ">
+            <li class="lg:mr-14 mr-2 ml-2 lg:text-3xl md:text-2xl text-xs">
+            <a class="text-gray-300 hover:underline hover:text-gray-200" href="/">Home</a>
+            </li>
+            <li class="lg:mr-14 mr-2 lg:text-3xl md:text-2xl text-xs">
+            <a class="text-gray-300 hover:underline hover:text-gray-200" href="/reviews">Reviews</a>
+            </li>
+            <li class="lg:mr-14 mr-2 lg:text-3xl md:text-2xl text-xs">
+            <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">New Movies</a>
+            </li>
+            <li class="lg:mr-14 mr-2 lg:text-3xl md:text-2xl text-xs">
+            <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">Top Movies</a>
+            </li>
+            <li class="lg:mr-14 mr-2 lg:text-3xl md:text-2xl text-xs">
+            <a class="text-gray-300 hover:underline hover:text-gray-200" href="#">Watchlist</a>
+            </li>
+        </ul>
+    </nav>
+
+
+
+
         <div class="container mx-auto px-4">
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <br>
-                        <h1 class="text-2xl">Reviews</h1>
+                        <h1 class="text-center lg:text-5xl text-2xl md:mb-56 mb-28 md:mt-40 mt-28">Reviews</h1>
                         @include('menu')
 
                         @if(session()->has('message'))
@@ -38,52 +61,37 @@
 
 @if ($reviews->count())
 @foreach ($reviews as $review)
+  @if($review->approve === 1)
   <div class="border-t border-gray-400">
     <dl>
       <div class="bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">
+        <dt class="lg:text-2xl text-sm font-medium text-gray-500">
           Movie:
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+        <dd class="mt-1 lg:text-2xl text-sm text-gray-900 sm:mt-0 sm:col-span-2">
             {{ $review->title->name }}&nbsp;({{ $review->title->publ_date }})
         </dd>
       </div>
 
-
-
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">
-          Written by:
-        </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            {{$review->user->name}} &nbsp;({{$review->user->role->name}})
-        </dd>
-      </div>
-
       <div class="bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">
-          Review:
+        <dt class="lg:text-2xl text-sm font-medium text-gray-500">
+          {{$review->user->name}}
+          <p class="text-sm text-gray-500 mt-8">Date: {{ $review->created_at->format('Y-m-d')}}</p>
         </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            {{ $review->body }}
-        </dd>
-      </div>
-
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">
-          Date:
-        </dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            {{$review->created_at}}
+        <dd class="mt-1 lg:text-2xl text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            {{-- {{ $review->body }} --}}           
+              {{$review->body}}          
         </dd>
       </div>
 
     </dl>
   </div><br /><br />
+    @endif
   @endforeach
+  <div>
 
   @else
-      <p>there are no reviews</p>
+      <p>There are no reviews yet</p>
   @endif
 </div>
 <br /><br />
